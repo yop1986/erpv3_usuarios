43bd1e29940de6f982f2c2e9092b5d6aa7521635
Pablo Godoy <pablodavid36@gmail.com>
2023-10-11 17:10:10 -0600

    personal_views.py

--- templatetags/fields_tags.py
+++ templatetags/fields_tags.py
@@ -12,15 +12,26 @@ def verbose_name_plural(value):
     return value._meta.verbose_name_plural
 
 @register.simple_tag
-def get_verbose_field_name(instance, field_name):
-    """
-    Returns verbose_name for a field.
-    """
-    return instance._meta.get_field(field_name).verbose_name.title()
-
-@register.simple_tag
 def get_descripcion_estados(value, tipo=1):
     if tipo == 1:
         return _('Activo') if value else _('Inactivo')
     else:
         return _('Vigente') if value else _('No vigente')
+
+
+# Utilizado en:
+#   - QlikSense
+@register.simple_tag
+def get_verbose_field_name(instance, field_name):
+    '''
+    Returns verbose_name for a field.
+    '''
+    return instance._meta.get_field(field_name).verbose_name.title()
+
+@register.simple_tag
+def get_object_value(object, field):
+    '''
+    Se utiliza para obtener los valores de un objeto de forma dinámica
+    '''
+    return getattr(object, field)
+
